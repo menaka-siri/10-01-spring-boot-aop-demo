1552b44b57ef62d46fee3bd6ff35fe1a8c408396
Menaka S <menaka442@gmail.com>
2025-02-11 23:59:26 +0530

    add forDaoPackageNoGetterSetter() pointcut

--- aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -17,19 +17,36 @@ public class MyDemoLoggingAspect {
 
     }
 
-//    @Before("execution(public void com.example.aopdemo.dao.AccountDAO.addAccount())") // inside the brackets is the pointcut
+    // create a pointcut for getter methods
+    @Pointcut("execution(* com.example.aopdemo.dao.*.get*(..))")
+    private void getter() {
+    }
+
+    // create a pointcut for setter methods
+    @Pointcut("execution(* com.example.aopdemo.dao.*.set*(..))")
+    private void setter() {
+    }
+
+    // create pointcut: include package ... exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {
+    }
+
+
+    //    @Before("execution(public void com.example.aopdemo.dao.AccountDAO.addAccount())") // inside the brackets is the pointcut
 //    @Before("execution(public void add*())")
 //    @Before("execution(* add*())")
 //    @Before("execution(* add*(com.example.aopdemo.Account))")
 //    @Before("execution(* add*(com.example.aopdemo.Account, ..))")
 //    @Before("execution(* add*(..))")
 //    @Before("execution(* com.example.aopdemo.dao.*.*(..))")
-    @Before("forDaoPackage()")
+//    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() { // this method can be any name
         System.out.println("\n=========>>> Executing @Before advice on method");
     }
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void performApiAnalytics() {
         System.out.println("\n=========>>> Perform API analytics");
     }
