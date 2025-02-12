a50bde35accbfd7e60941d4dcfe950593b025894
Menaka S <menaka442@gmail.com>
2025-02-12 13:49:26 +0530

    create separate class for each aop advice

---
+++ aopdemo/src/main/java/com/example/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -0,0 +1,15 @@
+package com.example.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyCloudLogAsyncAspect {
+
+    @Before("forDaoPackageNoGetterSetter()")
+    public void logToCloudAsync() {
+        System.out.println("\n=========>>> Logging to cloud in async fashion");
+    }
+}
--- aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,31 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-    // this is where we add all of our related advices for logging
-
-    // let's start with an @Before advice
-    @Pointcut("execution(* com.example.aopdemo.dao.*.*(..))")
-    private void forDaoPackage() {
-
-    }
-
-    // create a pointcut for getter methods
-    @Pointcut("execution(* com.example.aopdemo.dao.*.get*(..))")
-    private void getter() {
-    }
-
-    // create a pointcut for setter methods
-    @Pointcut("execution(* com.example.aopdemo.dao.*.set*(..))")
-    private void setter() {
-    }
-
-    // create pointcut: include package ... exclude getter/setter
-    @Pointcut("forDaoPackage() && !(getter() || setter())")
-    private void forDaoPackageNoGetterSetter() {
-    }
-
-
-    //    @Before("execution(public void com.example.aopdemo.dao.AccountDAO.addAccount())") // inside the brackets is the pointcut
+//    @Before("execution(public void com.example.aopdemo.dao.AccountDAO.addAccount())") // inside the brackets is the pointcut
 //    @Before("execution(public void add*())")
 //    @Before("execution(* add*())")
 //    @Before("execution(* add*(com.example.aopdemo.Account))")
@@ -44,10 +20,5 @@ public class MyDemoLoggingAspect {
     @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() { // this method can be any name
         System.out.println("\n=========>>> Executing @Before advice on method");
-    }
-
-    @Before("forDaoPackageNoGetterSetter()")
-    public void performApiAnalytics() {
-        System.out.println("\n=========>>> Perform API analytics");
     }
 }
