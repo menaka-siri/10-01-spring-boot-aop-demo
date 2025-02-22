a7f8952885bb9baa5e2daaa2fe00eaa40fe6d0fd
Menaka S <menaka442@gmail.com>
2025-02-22 17:19:23 +0530

    add Around advice

--- aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.example.aopdemo.aspect;
 
 import com.example.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
@@ -13,6 +14,31 @@ import java.util.List;
 @Component
 @Order(100)
 public class MyDemoLoggingAspect {
+
+    @Around("execution(* com.example.aopdemo.service.*.getFortune(..))")
+    public Object aroundGetFortune(
+            ProceedingJoinPoint theProceedingJoinPoint) throws Throwable {
+
+        // print out method we are advising
+        // print out which method we are advising on
+        String method = theProceedingJoinPoint.getSignature().toString();
+        System.out.println("\n=====>>> Executing @Around on method: " + method);
+
+        // get begin timestamp
+        long begin = System.currentTimeMillis();
+
+        // now, let's execute the method
+        Object result = theProceedingJoinPoint.proceed();
+
+        // get end timestamp
+        long end = System.currentTimeMillis();
+
+        //compute duration and display it
+        long duration = end - begin;
+        System.out.println("\n======> Duration: " + duration / 1000 + "seconds");
+
+        return result;
+    }
 
     @After("execution(* com.example.aopdemo.dao.AccountDAO.findAccounts(..))")
     public void afterFinallyFindAccountAdvice(JoinPoint theJoinPoint) {
