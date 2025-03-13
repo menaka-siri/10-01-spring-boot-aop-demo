c203c508a48b99d516582760e53862701e8da210
Menaka S <menaka442@gmail.com>
2025-03-13 09:48:39 +0530

    throw the exception caught in @Around advice

--- aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
+++ aopdemo/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -37,7 +37,9 @@ public class MyDemoLoggingAspect {
             System.out.println(e.getMessage());
 
             //give user a custom message
-            result = "Major accident! But no worries, your private AOP helicopter is on the way!";
+            // result = "Major accident! But no worries, your private AOP helicopter is on the way!";
+
+            throw e;
         }
 
         // get end timestamp
